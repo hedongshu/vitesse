4fe089f8a0e9445eaa7d30c5854cfbc472ca66c8
hds <hedongshu@foxmail.com>
2022-03-02 13:10:39 +0800

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,21 @@
-import { ViteSSG } from 'vite-ssg'
-import generatedRoutes from 'virtual:generated-pages'
-import { setupLayouts } from 'virtual:generated-layouts'
+import 'virtual:windi-base.css'
+import 'virtual:windi-components.css'
+// 你自定义的 css
+import './styles/main.css'
+
+import 'virtual:windi-utilities.css'
+import 'virtual:windi-devtools'
+
 import App from './App.vue'
 
-import '@unocss/reset/tailwind.css'
-import './styles/main.css'
-import 'uno.css'
+const app = createApp(App)
 
-const routes = setupLayouts(generatedRoutes)
+// 插件自动加载
+const modules = import.meta.globEager('./modules/*.ts')
+Object.values(modules).forEach(v => {
+	if (typeof v.default === 'function') {
+		v.default(app)
+	}
+})
 
-// https://github.com/antfu/vite-ssg
-export const createApp = ViteSSG(
-  App,
-  { routes, base: import.meta.env.BASE_URL },
-  (ctx) => {
-    // install all modules under `modules/`
-    Object.values(import.meta.globEager('./modules/*.ts')).forEach(i => i.install?.(ctx))
-  },
-)
+app.mount('#app')
